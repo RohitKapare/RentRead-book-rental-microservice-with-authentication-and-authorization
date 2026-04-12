c197744e1c569e7da026dcace531169f46340dfe
Rohit <rakapare86@gmail.com>
2026-04-12 18:02:21 +0530

    Mapper errors fixed

--- src/main/java/com/crio/rent_read/mapper/BookMapper.java
+++ src/main/java/com/crio/rent_read/mapper/BookMapper.java
@@ -5,16 +5,15 @@ import com.crio.rent_read.dto.request.BookUpdateRequest;
 import com.crio.rent_read.dto.response.BookResponse;
 import com.crio.rent_read.entity.Book;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface BookMapper {
 
-  BookMapper INSTANCE = Mappers.getMapper(BookMapper.class);
-
   BookResponse toBookResponse(Book book);
 
+  @Mapping(target = "id", ignore = true)
   Book toBook(BookRequest bookRequest);
 
   void updateBookFromDto(BookUpdateRequest dto, @MappingTarget Book book);
--- src/main/java/com/crio/rent_read/mapper/UserMapper.java
+++ src/main/java/com/crio/rent_read/mapper/UserMapper.java
@@ -5,16 +5,13 @@ import com.crio.rent_read.dto.response.UserResponse;
 import com.crio.rent_read.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-  UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
-
   UserResponse toUserResponse(User user);
 
   @Mapping(target = "id", ignore = true)
-  @Mapping(target = "authorities", ignore = true)
   User toUser(SignupRequest signupRequest);
+
 }
